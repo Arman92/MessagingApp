511627437ec6024bb2a6a2e6094779b913c49848
Arman <arman1371@gmail.com>
2021-07-01 02:00:09 +0430

    Add username and email regex

---
+++ backend/src/utils/regex.ts
@@ -0,0 +1,4 @@
+/* eslint-disable no-useless-escape */
+export const emailRegex =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+export const usernameRegex = /^[a-zA-Z0-9._]{4,}$/;
