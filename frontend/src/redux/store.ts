7ee5dbe4a949894dd13be2e604477a71918f4d86
Arman <arman1371@gmail.com>
2021-07-02 06:25:49 +0430

    Add saga store config

---
+++ frontend/src/redux/store.ts
@@ -0,0 +1,29 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+
+import { rootSaga } from './sagas';
+import { authReducer } from './slices';
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+// declare middleware in this array
+const middlewares = [sagaMiddleware];
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+  middleware: [
+    ...middlewares,
+    ...getDefaultMiddleware({
+      serializableCheck: true,
+    }),
+  ],
+  devTools: process.env.NODE_ENV !== 'production',
+});
+
+// then run the saga
+sagaMiddleware.run(rootSaga);
+
+export default store;
