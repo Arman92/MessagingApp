2311d165dfa54dea5943cbe7ff7b36c25c7c255b
Arman <arman1371@gmail.com>
2021-07-02 06:26:51 +0430

    Add auth slice

---
+++ frontend/src/redux/slices/auth/authSlice.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-param-reassign */
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+import { StorageKeys, LocalStorage } from '@messaging/utils';
+import { IAuthState, IUser } from '@messaging/models';
+
+const initialState: IAuthState = {
+  user: JSON.parse(LocalStorage.getItem(StorageKeys.AuthUser)),
+  authInProcess: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    authInitiated(state) {
+      state.authInProcess = true;
+    },
+    authSuccessful(state, action: PayloadAction<IUser>) {
+      state.authInProcess = false;
+      state.user = action.payload;
+    },
+    authFailed(state, action: PayloadAction<IUser>) {
+      state.authInProcess = false;
+      state.user = action.payload;
+    },
+    logOut(state) {
+      state.user = null;
+    },
+  },
+});
+
+export const { authInitiated, authSuccessful, authFailed, logOut } = authSlice.actions;
+
+export const authReducer = authSlice.reducer;
